56e06e748f8e602d1f2c8c204f1dfec1a46aeed0
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-03-29 12:16:48 -0400

    Add default settings

---
+++ client/src/constants.ts
@@ -0,0 +1,12 @@
+import {SettingsType} from "./types.tsx";
+
+export const DEFAULT_SETTINGS: SettingsType = {
+    userId: "PLACEHOLDER",
+    timerPomodoro: 25,
+    timerShortBreak: 5,
+    timerLongBreak: 15,
+    autoStartBreaks: false,
+    autoStartPomodoros: false,
+    longBreakInterval: 4
+}
+
