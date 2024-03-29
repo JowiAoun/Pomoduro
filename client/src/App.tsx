e85e44ee79eb57c756ff85273d69eaf0bb77a6af
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-03-29 12:17:04 -0400

    Add loading stage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,39 @@
-import { AppShell, Container, MantineProvider } from "@mantine/core";
+import { AppShell, Container, MantineProvider, Text } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "../styles/classes.css";
 import Header from "./Header.tsx";
 import TimerCard from "./TimerCard.tsx";
 import TaskSection from "./TaskSection.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TimerEnum } from "./enums.tsx";
+import { SettingsType, TaskType } from "./types.tsx";
+import { DEFAULT_SETTINGS } from "./constants.ts";
+
+const USER_ID: string = "2";
 
 function App() {
-  const userData = getUserData();
+  const [isLoading, setIsLoading] = useState(true);
+  const [settings, setSettings] = useState<SettingsType>(DEFAULT_SETTINGS);
+  const [tasks, setTasks] = useState<TaskType[]>([]);
   const [timerType, setTimerType] = useState(TimerEnum.Pomodoro);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const settingsResponse = await LOCALfetchSettings();
+        setSettings(settingsResponse);
+
+        const tasksResponse = await LOCALfetchTasks();
+        setTasks(tasksResponse);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setTimeout(() => setIsLoading(false), 0);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   function getAppVariant() {
     switch (timerType) {
@@ -22,18 +46,93 @@ function App() {
     }
   }
 
-  return (
+  async function fetchSettings(): Promise<SettingsType> {
+    const response = await fetch("http://localhost:8080/api/settings", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        userId: USER_ID,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("ERROR: Could not fetch tasks");
+    }
+
+    return response.json();
+  }
+
+  async function fetchTasks(): Promise<TaskType[]> {
+    const response = await fetch("http://localhost:8080/api/tasks", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        userId: USER_ID,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("ERROR: Could not fetch tasks");
+    }
+
+    return response.json();
+  }
+
+  function LOCALfetchSettings(): SettingsType {
+    return {
+      userId: "2",
+      timerPomodoro: 25,
+      timerShortBreak: 5,
+      timerLongBreak: 15,
+      autoStartBreaks: false,
+      autoStartPomodoros: false,
+      longBreakInterval: 4,
+    };
+  }
+
+  function LOCALfetchTasks(): TaskType[] {
+    return [
+      {
+        id: "2",
+        userId: "2",
+        title: "Task 1 for User 2",
+        note: "This is task 1 for user 2",
+        projectName: "Project 2",
+        done: false,
+        numCompleted: 0,
+        numToComplete: 3,
+        created: new Date().toISOString(),
+      },
+      {
+        id: "3",
+        userId: "2",
+        title: "Task 2 for User 2",
+        note: "This is task 2 for user 2",
+        projectName: "Project 2",
+        done: false,
+        numCompleted: 0,
+        numToComplete: 4,
+        created: new Date().toISOString(),
+      },
+    ];
+  }
+
+  return isLoading ? (
+    <MantineProvider>
+      <AppShell variant="timer-control-pomodoro">
+        <Container variant="loading" w="100%" h="100%">
+          <Text>Loading...</Text>
+        </Container>
+      </AppShell>
+    </MantineProvider>
+  ) : (
     <MantineProvider>
       <AppShell variant={getAppVariant()}>
         <Header></Header>
 
         <Container>
           <TimerCard
-            timerPomodoro={userData.timerPomodoro}
-            timerShortBreak={userData.timerShortBreak}
-            timerLongBreak={userData.timerLongBreak}
-            autoStartBreaks={userData.autoStartBreaks}
-            autoStartPomodoros={userData.autoStartPomodoros}
+            settings={settings}
             timerType={timerType}
             setTimerType={setTimerType}
           ></TimerCard>
@@ -41,48 +140,14 @@ function App() {
 
         <Container size="xs">
           <TaskSection
-            _tasks={userData.tasks}
-            timerPomodoro={userData.timerPomodoro}
-            timerShortBreak={userData.timerShortBreak}
-            timerLongBreak={userData.timerLongBreak}
-            longBreakInterval={userData.longBreakInterval}
+            _tasks={tasks}
+            settings={settings}
             timerType={timerType}
           ></TaskSection>
         </Container>
       </AppShell>
     </MantineProvider>
   );
-}
-
-function getUserData() {
-  return {
-    timerPomodoro: 25,
-    timerShortBreak: 5,
-    timerLongBreak: 15,
-    autoStartBreaks: false,
-    autoStartPomodoros: false,
-    longBreakInterval: 4,
-    tasks: [
-      {
-        id: "0",
-        title: "Linear Algebra I - Assignment 2",
-        numCompleted: 1,
-        numToComplete: 3,
-      },
-      {
-        id: "1",
-        title: "Chemistry II - Finish homework 1",
-        numCompleted: 0,
-        numToComplete: 2,
-      },
-      {
-        id: "2",
-        title: "Home - Go annoy Maria",
-        numCompleted: 3,
-        numToComplete: 5,
-      },
-    ],
-  };
 }
 
 export default App;
